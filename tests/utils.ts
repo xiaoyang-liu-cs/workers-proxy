29c53850ca09081bb0babd793c020c11bb93ace1
Xiaoyang Liu <siujoeng.lau@gmail.com>
2021-05-23 19:22:46 +0800

    Unit test coverage: utils.ts -> isMobile()
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

---
+++ tests/utils.ts
@@ -0,0 +1,33 @@
+import { isMobile } from '../src/utils';
+
+test('utils.ts -> isMobile()', async () => {
+  const userAgents: [string, boolean][] = [
+    [
+      // Pixel 3, Chrome 90
+      `
+      Mozilla/5.0 (Linux; Android 11; Pixel 3)
+      AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36
+      `,
+      true,
+    ],
+    [
+      // Macbook Pro, Chrome 90
+      `
+      Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)
+      AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36
+      `,
+      false,
+    ],
+    [
+      // Google Crawler
+      `
+      Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
+      `,
+      false,
+    ],
+  ];
+
+  userAgents.forEach(([userAgent, result]) => {
+    expect(isMobile(userAgent)).toEqual(result);
+  });
+});
