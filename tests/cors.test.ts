4b91a390d50df7577491342c52fbc2370b6e8cdc
Xiaoyang Liu <siujoeng.lau@gmail.com>
2021-05-25 18:11:01 +0800

    Write unit test for cors.transformResponse()
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- tests/cors.test.ts
+++ tests/cors.test.ts
@@ -2,14 +2,12 @@ import CORS from '../src/cors';
 
 describe('cors.ts -> transformResponse()', () => {
   test('Access-Control-Allow-Origin', async () => {
-    const headers = new Headers({
-      origin: 'https://httpbin.org',
-    });
-
     const request = new Request(
       'https://httpbin.org/post',
       {
-        headers,
+        headers: new Headers({
+          origin: 'https://httpbin.org',
+        }),
         method: 'GET',
       },
     );
@@ -27,6 +25,13 @@ describe('cors.ts -> transformResponse()', () => {
         origins: '*',
       },
     );
+    const asteriskOriginResponse = corsAsteriskOrigin.transformResponse(
+      request,
+      response,
+    );
+    expect(
+      asteriskOriginResponse.headers.get('Access-Control-Allow-Origin'),
+    ).toEqual('*');
 
     const corsExactOrigin = new CORS(
       {
@@ -36,23 +41,106 @@ describe('cors.ts -> transformResponse()', () => {
         ],
       },
     );
-
-    const asteriskOriginResponse = corsAsteriskOrigin.transformResponse(
-      request,
-      response,
-    );
-
     const exactOriginResponse = corsExactOrigin.transformResponse(
       request,
       response,
     );
-
-    expect(
-      asteriskOriginResponse.headers.get('Access-Control-Allow-Origin'),
-    ).toEqual('*');
-
     expect(
       exactOriginResponse.headers.get('Access-Control-Allow-Origin'),
     ).toEqual('https://httpbin.org');
+  });
+
+  test('Access-Control-Allow-Methods', async () => {
+    const request = new Request(
+      'https://httpbin.org/post',
+      {
+        headers: new Headers({
+          origin: 'https://httpbin.org',
+        }),
+        method: 'GET',
+      },
+    );
+    const response = new Response(
+      'Test response body',
+      {
+        headers: new Headers(),
+        status: 200,
+      },
+    );
+
+    const corsAsteriskMethods = new CORS(
+      {
+        origins: [
+          'https://httpbin.org',
+          'https://example.com',
+        ],
+        methods: '*',
+      },
+    );
+    const asteriskMethodsResponse = corsAsteriskMethods.transformResponse(
+      request,
+      response,
+    );
+    expect(
+      asteriskMethodsResponse.headers.get('Access-Control-Allow-Methods'),
+    ).toEqual('*');
+
+    const corsExactMethods = new CORS(
+      {
+        origins: [
+          'https://httpbin.org',
+          'https://example.com',
+        ],
+        methods: [
+          'GET',
+          'POST',
+          'PATCH',
+        ],
+      },
+    );
+    const exactMethodsResponse = corsExactMethods.transformResponse(
+      request,
+      response,
+    );
+    expect(
+      exactMethodsResponse.headers.get('Access-Control-Allow-Methods'),
+    ).toEqual('GET,POST,PATCH');
+  });
+
+  test('Access-Control-Max-Age', async () => {
+    const request = new Request(
+      'https://httpbin.org/post',
+      {
+        headers: new Headers({
+          origin: 'https://httpbin.org',
+        }),
+        method: 'GET',
+      },
+    );
+    const response = new Response(
+      'Test response body',
+      {
+        headers: new Headers(),
+        status: 200,
+      },
+    );
+
+    const corsMaxAge = new CORS(
+      {
+        origins: [
+          'https://httpbin.org',
+          'https://example.com',
+        ],
+        methods: '*',
+        maxAge: 86400,
+      },
+    );
+    const maxAgeResponse = corsMaxAge.transformResponse(
+      request,
+      response,
+    );
+    expect(
+      maxAgeResponse.headers.get('Access-Control-Max-Age'),
+    ).toEqual('86400');
   });
 });
